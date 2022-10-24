78460f0590bf8882b02084c3162e28b6076d06ea
Akash <na.akash715@gmail.com>
2022-10-25 03:53:23 +0600

    courses link setup

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Code Monkey Api Running!');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
